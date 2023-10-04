32611ffa76e10914cecacb7e13a3d54c0a93a8d6
junu1229 <junu1229@gmail.com>
2023-10-04 18:50:27 +0900

    feat: 구글맵 연동 및 타입스크립트 활용법, 코드 작성 팁

--- 3장/src/Company.ts
+++ 3장/src/Company.ts
@@ -1,17 +1,18 @@
 import { faker } from "@faker-js/faker";
 
-export class User {
-  name: string;
+export class Company {
+  companyName: string;
+  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
-
   constructor() {
-    this.name = faker.name.firstName();
+    this.companyName = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: faker.address.latitude(),
       lng: faker.address.longitude()
-    };
+    }
   }
 }
---
+++ 3장/src/CustomMap.ts
@@ -0,0 +1,52 @@
+
+// Instructions to every others class
+// on how they can be argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there!'
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+
+  }
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng
+  //     }
+  //   });
+  // }
+}
+
